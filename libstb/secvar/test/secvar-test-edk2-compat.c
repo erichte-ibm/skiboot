39e09f39a2a91ce9243bbe6f014d630cbc055c52
Nayna Jain <nayna@linux.ibm.com>
2019-08-12 15:10:59 -0400

    secvar/backend: add edk2 derived key updates processing
    As part of secureboot key management, the scheme for key updates
    processing is derived from tianocore reference implementation[1].
    This includes the verification of key updates signed in the form of
    PKCS7 structure.
    
    This patch adds the PKCS7 verification support for the signed
    updates processed by the user. It also adds the preprocessing code
    which initializes the empty non-volatile variables.
    
    This patch is still a work-in-progress, for example. it still needs
    to add the support for post-processing steps and better failure handling.
    
    V2:
     - fixed memcpy based on sizeof(keylen) rather than the value
     - added version and compatible values to driver struct
     - renamed to edk2-compat
    
    V4:
     - added draft document for general concepts
     - stores PK in the tpm
     - empty variables don't attempt to preallocate space
     - many malloc->zalloc changes
     - removed hard coded example variables
     - removed most printfs, converted some to prlog
     - updated for new pkcs7 implementation
     - removed unnecessary "SecureBoot", and "SecureMode"
         variables, handled by devtree entry
    
    [1] https://github.com/tianocore/edk2-staging.git
    
    <DELETE ME>
    SQUASH: secvar/test: add WIP edk2-compat test
    changes to make edk2 test cases and edk2 working.
    
    This mainly fixes the key formatting and reverts the edk2
    attributes value to 0.
    
    edk2 code cleanups
    edk2 test fixups
    fixed db test case
    cleanuped test cases.
    </DELETE ME>
    
    Signed-off-by: Nayna Jain <nayna@linux.ibm.com>

---
+++ libstb/secvar/test/secvar-test-edk2-compat.c
@@ -0,0 +1,178 @@
+/* Copyright 2019 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "secvar_common_test.c"
+#include "../backend/edk2-compat/edk2-compat.c"
+#include "../secvar_util.c"
+#include "../secvar_tpmnv.c"
+#include "../../crypto/pkcs7/pkcs7.c"
+#include <platform.h>
+#include "edk2_test_data.h"
+
+const char *secvar_test_name = "edk2-compat";
+
+struct platform platform;
+
+int secvar_set_secure_mode(void) { return 0; };
+
+int run_test()
+{
+	int rc = -1;
+	struct secvar_node *tmp;
+	int i;
+	int keksize;
+	int dbsize;
+
+	// TODO: actually exercise the PK->TPM logic at some point
+	proc_gen = 0;
+
+	// Check pre-process creates the empty variables
+	ASSERT(0 == list_length(&variable_bank));
+	rc = edk2_compat_pre_process();	
+	ASSERT(OPAL_SUCCESS == rc);
+	ASSERT(4 == list_length(&variable_bank));
+
+	// Add PK to update and .process()
+	printf("Add PK");
+	tmp = alloc_secvar(PK_auth_len);
+	memcpy(tmp->var->key, "PK", 3);
+	tmp->var->key_len = 3;
+	memcpy(tmp->var->data, PK_auth, PK_auth_len);
+	tmp->var->data_size = PK_auth_len;
+	list_add_tail(&update_bank, &tmp->link);
+	ASSERT(1 == list_length(&update_bank));
+
+	rc = edk2_compat_process();
+	ASSERT(OPAL_SUCCESS == rc);
+	ASSERT(4 == list_length(&variable_bank));
+	ASSERT(0 == list_length(&update_bank));
+	tmp = find_secvar("PK", 3, &variable_bank);
+	ASSERT(NULL != tmp);
+	ASSERT(0 != tmp->var->data_size);
+	ASSERT(PK_auth_len > tmp->var->data_size); // esl should be smaller without auth
+	ASSERT(!is_setup_mode());
+
+	// Add db, should fail with no KEK
+	printf("Add db");
+	dbsize = sizeof(DB_auth);
+	tmp = alloc_secvar(dbsize);
+	memcpy(tmp->var->key, "db", 3);
+	tmp->var->key_len = 3;
+	memcpy(tmp->var->data, DB_auth, dbsize);
+	tmp->var->data_size = dbsize;
+	list_add_tail(&update_bank, &tmp->link);
+	ASSERT(1 == list_length(&update_bank));
+	
+	rc = edk2_compat_process();
+	ASSERT(OPAL_SUCCESS != rc);
+	ASSERT(4 == list_length(&variable_bank));
+	ASSERT(0 == list_length(&update_bank));
+	tmp = find_secvar("db", 3, &variable_bank);
+	ASSERT(NULL != tmp);
+	ASSERT(0 == tmp->var->data_size); // Should still be empty
+
+	printf("Add KEK");
+	// Add valid KEK, .process(), should succeed
+	
+	tmp = alloc_secvar(ValidKEK_auth_len);
+	memcpy(tmp->var->key, "KEK", 4);
+	tmp->var->key_len = 4;
+	memcpy(tmp->var->data, ValidKEK_auth, ValidKEK_auth_len);
+	tmp->var->data_size = ValidKEK_auth_len;
+	list_add_tail(&update_bank, &tmp->link);
+	ASSERT(1 == list_length(&update_bank));
+
+	rc = edk2_compat_process();
+	ASSERT(OPAL_SUCCESS == rc);
+	ASSERT(4 == list_length(&variable_bank));
+	ASSERT(0 == list_length(&update_bank));
+	tmp = find_secvar("KEK", 4, &variable_bank);
+	ASSERT(NULL != tmp);
+	ASSERT(0 != tmp->var->data_size);
+
+	// Add db, .process(), should succeed
+	printf("Add db again\n");
+	dbsize = sizeof(DB_auth);
+	tmp = alloc_secvar(dbsize);
+	memcpy(tmp->var->key, "db", 3);
+	tmp->var->key_len = 3;
+	memcpy(tmp->var->data, DB_auth, dbsize);
+	tmp->var->data_size = dbsize;
+	list_add_tail(&update_bank, &tmp->link);
+	ASSERT(1 == list_length(&update_bank));
+
+	rc = edk2_compat_process();
+	ASSERT(OPAL_SUCCESS == rc);
+	ASSERT(4 == list_length(&variable_bank));
+	ASSERT(0 == list_length(&update_bank));
+	tmp = find_secvar("db", 3, &variable_bank);
+	printf("tmp is %s\n", tmp->var->key);
+	ASSERT(NULL != tmp);
+	ASSERT(0 != tmp->var->data_size);
+
+	// Add invalid KEK, .process(), should fail 
+	printf("Add invalid KEK\n");
+	keksize = sizeof(InvalidKEK_auth);
+	tmp = alloc_secvar(keksize);
+	memcpy(tmp->var->key, "KEK", 4);
+	tmp->var->key_len = 4;
+	memcpy(tmp->var->data, InvalidKEK_auth, keksize);
+	tmp->var->data_size = keksize;
+	list_add_tail(&update_bank, &tmp->link);
+	ASSERT(1 == list_length(&update_bank));
+
+	rc = edk2_compat_process();
+	ASSERT(OPAL_SUCCESS != rc);
+	ASSERT(4 == list_length(&variable_bank));
+	ASSERT(0 == list_length(&update_bank));
+	tmp = find_secvar("KEK", 4, &variable_bank);
+	ASSERT(NULL != tmp);
+	ASSERT(0 != tmp->var->data_size);
+
+	// Add ill formatted KEK, .process(), should fail 
+	printf("Add invalid KEK\n");
+	keksize = sizeof(IllformatKEK_auth);
+	tmp = alloc_secvar(keksize);
+	memcpy(tmp->var->key, "KEK", 4);
+	tmp->var->key_len = 4;
+	memcpy(tmp->var->data, IllformatKEK_auth, keksize);
+	tmp->var->data_size = keksize;
+	list_add_tail(&update_bank, &tmp->link);
+	ASSERT(1 == list_length(&update_bank));
+
+	rc = edk2_compat_process();
+	ASSERT(OPAL_SUCCESS != rc);
+	ASSERT(4 == list_length(&variable_bank));
+	ASSERT(0 == list_length(&update_bank));
+	tmp = find_secvar("KEK", 4, &variable_bank);
+	ASSERT(NULL != tmp);
+	ASSERT(0 != tmp->var->data_size);
+
+	// Add garbage to .process()
+
+	return 0;
+}
+
+int main(void)
+{
+	int rc;
+
+	list_head_init(&variable_bank);
+	list_head_init(&update_bank);
+
+	rc = run_test();
+
+	return rc;
+}
