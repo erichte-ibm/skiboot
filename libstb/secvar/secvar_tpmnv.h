6012445fa27faf4ff776ebcaaebe7bf2d70d5c97
Eric Richter <erichte@linux.ibm.com>
2019-10-23 17:25:49 -0500

    secvar_tpmnv: add high-level tpm nv index abstraction for secvar
    Multiple components, like the storage driver or backend driver, may need
    to store information in the one TPM NV index assigned for secure boot.
    This abstraction provides a method for these components to share the
    index space without stomping on each other's data, and without them
    needing to understand anything about the other.
    
    This is probably an overengineered solution to the problem, but the
    intent is to keep the drivers as independent from one another as possible.
    
    NOTE: At the moment, this implementation does NOT actually utilize the
    TPM NV space. This patch depends on a forthcoming TSS integration to
    handle the TPM communication. For now, this patch uses extra space at
    the end of the SECBOOT PNOR partition as its simulated TPM NVRAM.
    Temporary code is marked in the comments accordingly.
    
    Signed-off-by: Eric Richter <erichte@linux.ibm.com>

---
+++ libstb/secvar/secvar_tpmnv.h
@@ -0,0 +1,16 @@
+#ifndef _SECVAR_TPMNV_H_
+#define _SECVAR_TPMNV_H_
+#include <stdint.h>
+
+extern int tpm_first_init;
+
+int secvar_tpmnv_alloc(uint32_t id, int32_t size);
+int secvar_tpmnv_read(uint32_t id, void *buf, size_t size, size_t off);
+int secvar_tpmnv_write(uint32_t id, void *buf, size_t size, size_t off);
+int secvar_tpmnv_size(uint32_t id);
+
+extern int tpm_fake_nv;
+extern uint64_t tpm_fake_nv_offset;
+
+#endif
+
