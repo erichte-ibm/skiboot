7d8b5a0b54d4429c79d71e9e92891a31066e3db4
Eric Richter <erichte@linux.ibm.com>
2020-01-09 11:14:12 -0600

    SQUASH: mbedtls-config.h add X509_CRL needed for pkcs7

--- libstb/crypto/mbedtls-config.h
+++ libstb/crypto/mbedtls-config.h
@@ -79,7 +79,7 @@
 #define MBEDTLS_SHA512_C
 #define MBEDTLS_X509_USE_C
 #define MBEDTLS_X509_CRT_PARSE_C
-//#define MBEDTLS_X509_CRL_PARSE_C
+#define MBEDTLS_X509_CRL_PARSE_C
 //#define MBEDTLS_CMAC_C
 
 /* Settings to reduce/remove warnings */
