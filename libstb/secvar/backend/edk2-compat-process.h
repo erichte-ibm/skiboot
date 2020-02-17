c60dc9f8a91ad99509b524bb574dc6e8bb75622b
Nayna Jain <nayna@linux.vnet.ibm.com>
2020-02-17 07:57:25 -0500

    Fixes feedbacks from Eric:
    Make functions static
    whitespace error
    comment
    
    Signed-off-by: Nayna Jain <nayna@linux.vnet.ibm.com>

--- libstb/secvar/backend/edk2-compat-process.h
+++ libstb/secvar/backend/edk2-compat-process.h
@@ -34,25 +34,6 @@ extern bool setup_mode;
 int update_variable_in_bank(struct secvar *secvar, const char *data,
 			    uint64_t dsize);
 
-/* Converts utf8 string to ucs2 */
-char *utf8_to_ucs2(const char *key, size_t keylen);
-
-/* Returns the authority that can sign the given key update */
-void get_key_authority(const char *ret[3], const char *key);
-
-/* Returns the size of the complete ESL. */
-int get_esl_signature_list_size(char *buf, size_t buflen);
-
-/* Copies the certificate from the ESL into cert buffer and returns the size
- * of the certificate
- */
-int get_esl_cert(char *buf, size_t buflen, char **cert);
-
-/* Extracts size of the PKCS7 signed data embedded in the
- * struct Authentication 2 Descriptor Header.
- */
-int get_pkcs7_len(struct efi_variable_authentication_2 *auth);
-
 /* This function outputs the Authentication 2 Descriptor in the
  * auth_buffer and returns the size of the buffer. Please refer to
  * edk2.h for details on Authentication 2 Descriptor
@@ -62,36 +43,11 @@ int get_auth_descriptor2(void *buf, size_t buflen, char **auth_buffer);
 /* Check the format of the ESL */
 int validate_esl_list(char *key, char *esl, size_t size);
 
-/* Get the timestamp for the last update of the give key */
-struct efi_time *get_last_timestamp(const char *key);
-
 /* Update the TS variable with the new timestamp */
 int update_timestamp(char *key, struct efi_time *timestamp);
 
 /* Check the new timestamp against the timestamp last update was done */
 int check_timestamp(char *key, struct efi_time *timestamp);
-
-/* Extract PKCS7 from the authentication header */
-int get_pkcs7(struct efi_variable_authentication_2 *auth,
-	      mbedtls_pkcs7 **pkcs7);
-
-/* Verify the PKCS7 signature on the signed data. */
-int verify_signature(struct efi_variable_authentication_2 *auth, char *newcert,
-		     size_t new_data_size, struct secvar *avar);
-
-/* Verify the PKCS7 signature on the signed data. */
-int process_single_update(struct secvar *auth);
-
-/* Create the single buffer
- * name || vendor guid || attributes || timestamp || newcontent
- * which is submitted as signed by the user.
- * Returns number of bytes in the new buffer, else negative error
- * code.
- */
-int get_data_to_verify(char *key, char *new_data,
-		size_t new_data_size,
-		char **buffer,
-		size_t *buffer_size, struct efi_time *timestamp);
 
 /* Check the GUID of the data type */
 bool is_pkcs7_sig_format(void *data);
