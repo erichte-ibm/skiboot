c34f689ad94acd07ee0db73cff565398275bdca7
Nayna Jain <nayna@linux.vnet.ibm.com>
2020-02-18 17:01:06 -0500

    Fix the "possibly a problem" reported by BEAM.
    It wasn't as such a problem in our case as we know the hierarchy
    and the supported variables. Still, it was good to follow the
    steps which keeps the code right in any case.
    
    -- ERROR7     /*accessing beyond memory*/     >>>ERROR7_get_key_authority_df92eaf1505
    "libstb/secvar/backend/edk2-compat-process.c", line 83: accessing beyond array bounds
    ONE POSSIBLE PATH LEADING TO THE ERROR:
     "libstb/secvar/backend/edk2-compat-process.c", line 77: the if-condition is true
     "libstb/secvar/backend/edk2-compat-process.c", line 79: the if-condition is true
     "libstb/secvar/backend/edk2-compat-process.c", line 81: the if-condition is true
     "libstb/secvar/backend/edk2-compat-process.c", line 83: assigning into `i'
     "libstb/secvar/backend/edk2-compat-process.c", line 83: the index `i++' is outside the bounds [0,2] of array `ret'.
    
     The index has value = 3.
    
     VALUES AT THE END OF THE PATH:
      i = 4
    
    Signed-off-by: Nayna Jain <nayna@linux.vnet.ibm.com>

--- libstb/secvar/backend/edk2-compat-process.c
+++ libstb/secvar/backend/edk2-compat-process.c
@@ -74,14 +74,15 @@ static void get_key_authority(const char *ret[3], const char *key)
 {
 	int i = 0;
 
-	if (key_equals(key, "PK"))
+	if (key_equals(key, "PK")) {
 		ret[i++] = "PK";
-	if (key_equals(key, "KEK"))
+	} else if (key_equals(key, "KEK")) {
 		ret[i++] = "PK";
-	if (key_equals(key, "db") || key_equals(key, "dbx")) {
+	} else if (key_equals(key, "db") || key_equals(key, "dbx")) {
 		ret[i++] = "KEK";
 		ret[i++] = "PK";
 	}
+
 	ret[i] = NULL;
 }
 
