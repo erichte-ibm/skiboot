1d723c23681d01083bd39cb80c7c30441f5ede1e
Eric Richter <erichte@linux.ibm.com>
2020-02-26 15:12:32 -0600

    SQUASH: secboot_tpm: update for new TSS changes

--- libstb/secvar/storage/fakenv_ops.c
+++ libstb/secvar/storage/fakenv_ops.c
@@ -3,32 +3,30 @@
 
 static size_t fakenv_offset = sizeof(struct secboot);
 
-static int fakenv_read(uint32_t nvIndex, void *buf, size_t bufsize, uint64_t off)
+static int fakenv_read(TPMI_RH_NV_INDEX nvIndex, void *buf,
+                     size_t bufsize,  uint16_t off)
 {
 	(void) nvIndex;
 	(void) off;
 	return platform.secboot_read(buf, fakenv_offset, bufsize);
 }
 
-static int fakenv_write(uint32_t nvIndex, void *buf, size_t bufsize, uint64_t off)
+static int fakenv_write(TPMI_RH_NV_INDEX nvIndex, void *buf,
+                      size_t bufsize, uint16_t off)
 {
 	(void) nvIndex;
 	(void) off;
 	return platform.secboot_write(fakenv_offset, buf, bufsize);
 }
 
-static int fakenv_definespace(uint32_t nvIndex, const char hierarchy,
-			const char hierarchy_authorization,
-			uint16_t dataSize)
+static int fakenv_definespace(TPMI_RH_NV_INDEX nvIndex, uint16_t dataSize)
 {
 	(void) nvIndex;
-	(void) hierarchy;
-	(void) hierarchy_authorization;
 	(void) dataSize;
 	return 0;
 }
 
-static int fakenv_writelock(uint32_t nvIndex)
+static int fakenv_writelock(TPMI_RH_NV_INDEX nvIndex)
 {
 	(void) nvIndex;
 	return 0;
